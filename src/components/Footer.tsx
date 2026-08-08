4c82a128e573c8aaeb70ea6226d80dff92ed200f
thienky0104 <thienky01042000@gmail.com>
2026-08-08 20:28:13 +0700

    Updated Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -14,7 +14,7 @@ export default function Footer() {
       style={{
         backgroundImage: `url(${FOOTER_BG})`,
         backgroundSize: 'cover',
-        backgroundPosition: 'center 45%',
+        backgroundPosition: 'center 35%',
         backgroundRepeat: 'no-repeat',
       }}
     >
